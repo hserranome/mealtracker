a92e34ec67da51b0b603696e9bd98d4ace3ff3cd
hserranome <hendy@hserrano.me>
2024-08-07 03:24:27 +0200

    wip

--- data/types/Food.ts
+++ data/types/Food.ts
@@ -1,18 +1,36 @@
+export type Nutriments = {
+  kcal: number;
+  fat: number;
+  proteins: number;
+  carbohydrates: number;
+};
+
 export type Food = {
   name: string;
   brand: string;
   code: string;
-  meal: number; //index
+  nutriments: Nutriments;
+  images_url?: string;
+  images_thumb_url?: string;
+  images_ingredients?: string;
+};
+
+export type Ingredient = {
   amount: number;
-  image_url?: string;
-  image_thumb_url?: string;
-  image_ingredients?: string;
   unit: string;
-  nutriment_basis: string;
-  nutriments: {
-    kcal: number;
-    fat: number;
-    proteins: number;
-    carbohydrates: number;
-  };
+  food: Food;
+};
+
+export type Recipe = {
+  name: string;
+  description: string;
+  instructions: string;
+  ingredients: Ingredient[];
+};
+
+export type DairyItem = {
+  meal: number;
+  amount: number;
+  unit: string;
+  item: Food | Recipe;
 };
