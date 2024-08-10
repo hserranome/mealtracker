0f1eaa2a338c037ad74d7c060dbc692866efb9bd
Hendy Serrano <hendy@hserrano.me>
2024-08-10 11:06:58 +0200

    wip: initial login screen

---
+++ app/login/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+import { OnboardingScreenContainer } from '~/components/OnboardingScreenContainer';
+import { Button, ButtonType } from '~/components/Button';
+
+const LoginScreen = () => {
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <OnboardingScreenContainer title="Login" progress={null}>
+      <View style={styles.container}>
+        <Button title="Login with Email" style={styles.button} />
+        <Button title="Login with Google" type={ButtonType.Outline} style={styles.button} />
+        <Button title="Login with Apple" type={ButtonType.Outline} style={styles.button} />
+      </View>
+    </OnboardingScreenContainer>
+  );
+};
+
+const stylesheet = createStyleSheet((theme) => ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: theme.margins[16],
+  },
+  button: {
+    width: '100%',
+    marginBottom: theme.margins[16],
+  },
+}));
+
+export default LoginScreen;
