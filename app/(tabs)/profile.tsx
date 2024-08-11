4366f32615a08fcca4651b473d2d16b117769ac6
Hendy Serrano <hendy@hserrano.me>
2024-08-11 14:31:12 +0200

    feat: add logout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,22 +1,33 @@
 import { useRouter } from 'expo-router';
 import { Text, View } from 'react-native';
 
+import { usePocketbase } from '~/components/contexts/PocketbaseContext';
 import { Button } from '~/components/elements/Button';
 import { db } from '~/data/database';
 import { users } from '~/data/schemas';
 
 export default function Profile() {
   const router = useRouter();
+  const { logout } = usePocketbase();
 
   const wipeData = async () => {
     await db.delete(users);
     router.replace('/');
   };
 
+  const handleLogout = async () => {
+    try {
+      await logout?.();
+    } finally {
+      router.replace('/');
+    }
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile</Text>
       <Button onPress={() => wipeData()} title="Wipe data" />
+      <Button onPress={handleLogout} title="Logout" />
     </View>
   );
 }
