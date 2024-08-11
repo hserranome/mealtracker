d184405400e9ab853bd792a53f09af0dd788f185
Hendy Serrano <hendy@hserrano.me>
2024-08-11 19:19:35 +0200

    chore: types and remove forwardRef

--- components/elements/TextInput/TextInput.tsx
+++ components/elements/TextInput/TextInput.tsx
@@ -1,5 +1,5 @@
-import { ComponentProps, forwardRef } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { ComponentProps } from 'react';
+import { Controller, ControllerRenderProps, useFormContext } from 'react-hook-form';
 import { TextInput as RNTextInput, Text, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
@@ -9,34 +9,28 @@ export type TextInputProps = {
   name?: string;
 } & ComponentProps<typeof RNTextInput>;
 
-export const TextInput = forwardRef<RNTextInput, TextInputProps>(
-  ({ suffix, containerStyle, name, ...props }, ref) => {
-    const { control } = useFormContext();
-    const { styles, theme } = useStyles(stylesheet);
-    const renderInput = (field) => (
-      <View style={[styles.container, containerStyle]} key={name}>
-        <RNTextInput
-          placeholderTextColor={theme.colors.base600}
-          ref={ref}
-          style={[styles.input, props.style]}
-          {...props}
-          {...field}
-        />
-        {suffix && <Text style={styles.suffix}>{suffix}</Text>}
-      </View>
-    );
+export const TextInput = ({ suffix, containerStyle, name, ...props }: TextInputProps) => {
+  const { control } = useFormContext();
+  const { styles, theme } = useStyles(stylesheet);
 
-    return name ? (
-      <Controller
-        control={control}
-        name={name}
-        render={({ field }) => renderInput(field)}
+  const renderInput = (field: ControllerRenderProps | RNTextInput['props']) => (
+    <View style={[styles.container, containerStyle]} key={name}>
+      <RNTextInput
+        placeholderTextColor={theme.colors.base600}
+        style={[styles.input, props.style]}
+        {...props}
+        {...field}
       />
-    ) : (
-      renderInput(props)
-    );
-  }
-);
+      {suffix && <Text style={styles.suffix}>{suffix}</Text>}
+    </View>
+  );
+
+  return name ? (
+    <Controller control={control} name={name} render={({ field }) => renderInput(field)} />
+  ) : (
+    renderInput(props)
+  );
+};
 
 const stylesheet = createStyleSheet((theme) => ({
   container: {
