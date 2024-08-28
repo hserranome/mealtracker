57735f9e0cd5bd6192c00f0e06f896964f3c944b
Hendy Serrano <hendy@hserrano.me>
2024-08-28 21:34:41 +0200

    change: unify goals schemas

--- data/schemas/goals.ts
+++ data/schemas/goals.ts
@@ -2,16 +2,19 @@ import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 import { users } from './users';
 
-export const weight_goals = sqliteTable('weight_goals', {
+export const goals = sqliteTable('goals', {
   id: integer('id').primaryKey({ autoIncrement: true }).unique().notNull(),
   user_id: integer('user_id')
     .notNull()
     .references(() => users.id),
-  height: integer('height'),
+  // TODO: implement bitmask instead of text?
+  high_days: text('high_days').notNull(),
+  calories_goal: integer('calories_goal').notNull(), // daily, then extrapolated
   initial_weight: real('initial_weight'),
   goal_weight: real('goal_weight'),
   weight_variance_rate: real('weight_variance_rate'),
+  estimated_end_date: text('estimated_end_date'),
   created_at: text('created_at').notNull().default('CURRENT_TIMESTAMP'),
 });
-export type WeightGoal = typeof weight_goals.$inferSelect;
-export type NewWeightGoal = typeof weight_goals.$inferInsert;
+export type Goal = typeof goals.$inferSelect;
+export type NewGoal = typeof goals.$inferInsert;
--- data/schemas/index.ts
+++ data/schemas/index.ts
@@ -4,5 +4,4 @@ export * from './dairies_items';
 export * from './foods';
 export * from './recipes';
 export * from './recipes_items';
-export * from './macro_goals';
-export * from './weight_goals';
+export * from './goals';
