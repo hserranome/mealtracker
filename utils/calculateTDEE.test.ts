5fdb8bb8c052ad2d3fd31175144db0604f402607
Hendy Serrano <hendy@hserrano.me>
2024-08-29 23:08:38 +0200

    change: implement advance tdee calculations

--- utils/calculateTDEE.test.ts
+++ utils/calculateTDEE.test.ts
@@ -1,107 +1,147 @@
 import {
   calculateBasalMetabolicRate,
-  calculateTDEE,
-  calculateBasalMetabolicRateArgs,
-  calculateTDEEArgs,
+  calculateSimpleBaseTDEE,
+  getDatesTdee,
+  getWeeksToGoal,
+  getTargetTdee,
+  calculateBaseTDEE,
 } from './calculateTDEE';
 
 import { Sex, ActivityLevel } from '~/data/types';
 
 describe('calculateBasalMetabolicRate', () => {
   it('should calculate BMR for a male', () => {
-    const args: calculateBasalMetabolicRateArgs = {
+    const result = calculateBasalMetabolicRate({
       sex: Sex.Male,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateBasalMetabolicRate(args);
+    });
     expect(result).toBe(1680);
   });
 
   it('should calculate BMR for a female', () => {
-    const args: calculateBasalMetabolicRateArgs = {
+    const result = calculateBasalMetabolicRate({
       sex: Sex.Female,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateBasalMetabolicRate(args);
+    });
     expect(result).toBe(1514);
   });
 
   it('should return 0 for an unknown sex', () => {
-    const args: calculateBasalMetabolicRateArgs = {
+    const result = calculateBasalMetabolicRate({
       sex: 'unknown' as unknown as Sex,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateBasalMetabolicRate(args);
+    });
     expect(result).toBe(0);
   });
 });
 
-describe('calculateTDEE', () => {
+describe('calculateBaseTDEE', () => {
   it('should calculate TDEE for a male with a sedentary activity level', () => {
-    const args: calculateTDEEArgs = {
+    const result = calculateBaseTDEE({
       activityLevel: ActivityLevel.Sedentary,
       sex: Sex.Male,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateTDEE(args);
+    });
     expect(result).toBe(2016);
   });
 
   it('should calculate TDEE for a female with a moderate activity level', () => {
-    const args: calculateTDEEArgs = {
+    const result = calculateBaseTDEE({
       activityLevel: ActivityLevel.Moderate,
       sex: Sex.Female,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateTDEE(args);
+    });
     expect(result).toBe(2347);
   });
 
   it('should return 0 for an unknown activity level', () => {
-    const args: calculateTDEEArgs = {
+    const result = calculateBaseTDEE({
       activityLevel: 'unknown' as unknown as ActivityLevel,
       sex: Sex.Male,
       weight: 70,
       height: 180,
       age: 30,
-    };
-    const result = calculateTDEE(args);
+    });
     expect(result).toBe(0);
   });
+});
 
-  it('should return adjusted calories based on weight change positive', () => {
-    const args: calculateTDEEArgs = {
-      activityLevel: ActivityLevel.Sedentary,
-      sex: Sex.Male,
-      weight: 70,
-      height: 180,
-      age: 30,
-      weightChangePerWeek: 0.5,
-    };
-    const result = calculateTDEE(args);
-    expect(result).toBe(2516);
+describe('calculateSimpleBaseTDEE', () => {
+  it('should return correct value', () => {
+    const result = calculateSimpleBaseTDEE(70);
+    expect(result).toBe(2005);
+  });
+});
+
+describe('getWeeksToGoal', () => {
+  it('should return correct value', () => {
+    const result = getWeeksToGoal(70, 80, 0.5);
+    expect(result).toBe(20);
+  });
+});
+
+describe('getTargetTDEE', () => {
+  it('should return correct value', () => {
+    const result = getTargetTdee(70, 72, 1575, 275);
+    expect(result).toBe(1850);
+  });
+});
+
+describe('getDatesTdee', () => {
+  it('should return correct value', () => {
+    const dates = [
+      {
+        tdee: 2012,
+        weight: 70,
+      },
+      {
+        tdee: 2007,
+        weight: 70,
+      },
+      {
+        tdee: 2000,
+        weight: 70,
+      },
+      {
+        tdee: 1800,
+        weight: 70,
+      },
+      {
+        tdee: 1800,
+        weight: 70,
+      },
+      {
+        tdee: 2000,
+        weight: 70,
+      },
+      {
+        tdee: 2000,
+        weight: 70,
+      },
+    ];
+    const result = getDatesTdee(dates, 70);
+    expect(result).toBe(1945);
   });
 
-  it('should return adjusted calories based on weight change negative', () => {
-    const args: calculateTDEEArgs = {
-      activityLevel: ActivityLevel.Sedentary,
-      sex: Sex.Male,
-      weight: 70,
-      height: 180,
-      age: 30,
-      weightChangePerWeek: -0.5,
+  it('should return correct value', () => {
+    const day = {
+      weight: 72,
+      tdee: 2000,
     };
-    const result = calculateTDEE(args);
-    expect(result).toBe(1466);
+
+    const data = [day, day, day, day, day, day, day];
+
+    const result = getDatesTdee(data, 70, 1945, 2);
+    expect(result).toBe(870);
   });
 });
--- utils/calculateTDEE.ts
+++ utils/calculateTDEE.ts
@@ -1,14 +1,8 @@
 import { Sex, ActivityLevel } from '~/data/types';
 
-const caloriesPerKgLose = 7700 / 7;
-const caloriesPerKgGain = 7000 / 7;
-
-export type calculateBasalMetabolicRateArgs = {
-  sex: Sex;
-  weight: number;
-  height: number;
-  age: number;
-};
+const round5 = (num: number) => Math.round(num / 5) * 5;
+const round05 = (num: number) => Math.round(num / 0.5) * 0.5;
+const roundFirstDecimal = (num: number) => Math.round(num * 10) / 10;
 
 // Uses the Mifflin-St Jeor Equation
 export const calculateBasalMetabolicRate = ({
@@ -16,7 +10,12 @@ export const calculateBasalMetabolicRate = ({
   weight,
   height,
   age,
-}: calculateBasalMetabolicRateArgs) => {
+}: {
+  sex: Sex;
+  weight: number;
+  height: number;
+  age: number;
+}) => {
   if (sex === Sex.Male) {
     return 10 * weight + 6.25 * height - 5 * age + 5;
   }
@@ -26,16 +25,18 @@ export const calculateBasalMetabolicRate = ({
   return 0;
 };
 
-export type calculateTDEEArgs = {
-  activityLevel: ActivityLevel;
-  weightChangePerWeek?: number;
-} & calculateBasalMetabolicRateArgs;
-
-export const calculateTDEE = ({
+export const calculateBaseTDEE = ({
   activityLevel,
   weightChangePerWeek = 0,
   ...calculateBasalMetabolicRateArgs
-}: calculateTDEEArgs) => {
+}: {
+  activityLevel: ActivityLevel;
+  weightChangePerWeek?: number;
+  sex: Sex;
+  weight: number;
+  height: number;
+  age: number;
+}) => {
   const scale = {
     [ActivityLevel.Sedentary]: 1.2,
     [ActivityLevel.Light]: 1.375,
@@ -46,11 +47,61 @@ export const calculateTDEE = ({
   if (!scale[activityLevel]) return 0;
   const tdee = calculateBasalMetabolicRate(calculateBasalMetabolicRateArgs) * scale[activityLevel];
 
-  // Adjust calories based on weight change per week.
-  // Probably can be improved and made dynamic. Like on that CSV
-  const calorieAdjustment =
-    weightChangePerWeek * (weightChangePerWeek > 0 ? caloriesPerKgGain : caloriesPerKgLose);
-  const adjustedTDEE = tdee + calorieAdjustment;
+  return Math.round(tdee);
+};
 
-  return Math.round(adjustedTDEE);
+export const calculateSimpleBaseTDEE = (weight: number) => {
+  return round5(weight * 28.66006);
+};
+
+const magicNumber = 7716.17;
+
+export const getTargetDailyDiff = (weightVariancePerWeek: number) => {
+  return round5((weightVariancePerWeek * magicNumber) / 7);
+};
+
+export const getTargetTdee = (
+  currentWeight: number,
+  goalWeight: number,
+  baseTdee: number,
+  targetDailyDiff: number
+) => {
+  if (currentWeight === goalWeight) return baseTdee;
+  if (currentWeight < goalWeight) return baseTdee + targetDailyDiff;
+  if (currentWeight > goalWeight) return baseTdee - targetDailyDiff;
+  return NaN;
+};
+
+export const getWeeksToGoal = (
+  goalWeight: number,
+  currentWeight: number,
+  weightVariancePerWeek: number
+) => {
+  return Math.abs(round05((goalWeight - currentWeight) / weightVariancePerWeek));
+};
+
+type dayRecord = {
+  tdee: number;
+  weight: number;
+};
+export const getDatesTdee = (
+  days: dayRecord[],
+  initialWeight: number,
+  previousAverageTdee?: number, // max last 5 weeks averaged tdee sum
+  numberOfPreviousWeeks?: number
+) => {
+  const averageTdee = Math.round(days.reduce((a, b) => a + b.tdee, 0) / days.length);
+
+  const averageWeight = roundFirstDecimal(days.reduce((a, b) => a + b.weight, 0) / days.length);
+  const daysEntried = days.length;
+
+  const weightDiff = averageWeight - initialWeight;
+
+  const tdee = averageTdee + (-weightDiff * magicNumber) / daysEntried;
+
+  if (previousAverageTdee && numberOfPreviousWeeks) {
+    const adjustedTdee = (tdee + previousAverageTdee) / numberOfPreviousWeeks;
+    return round5(adjustedTdee);
+  }
+  return round5(tdee);
 };
