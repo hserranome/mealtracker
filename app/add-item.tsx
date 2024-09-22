d4c3668caa8f5ad52aa9f3b4b60cadc6ce9e9edd
Hendy Serrano <hendy@hserrano.me>
2024-09-22 16:00:19 +0200

    fix: rm missing stuff being used

--- app/add-item.tsx
+++ app/add-item.tsx
@@ -1,27 +1,25 @@
-import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
+// import { useLocalSearchParams } from 'expo-router';
+// import { useState } from 'react';
 import { View } from 'react-native';
 
-import { Food } from '~/data/types/Food';
-
-const getSafeString = (str: any) => (typeof str === 'string' ? str : '');
+// const getSafeString = (str: any) => (typeof str === 'string' ? str : '');
 
 export default function AddItem() {
-  const initialRawParams = useLocalSearchParams();
-  const initialParams: Food = {
-    name: getSafeString(initialRawParams.name),
-    brand: getSafeString(initialRawParams.brand),
-    code: getSafeString(initialRawParams.code),
-    meal: Number(initialRawParams.meal),
-    amount: Number(initialRawParams.amount),
-    unit: getSafeString(initialRawParams.unit),
-    nutriment_basis: getSafeString(initialRawParams.nutriment_basis),
-    nutriments: JSON.parse(getSafeString(initialRawParams.nutriments)),
-  };
+  // const initialRawParams = useLocalSearchParams();
+  // const initialParams: Food = {
+  //   name: getSafeString(initialRawParams.name),
+  //   brand: getSafeString(initialRawParams.brand),
+  //   code: getSafeString(initialRawParams.code),
+  //   meal: Number(initialRawParams.meal),
+  //   amount: Number(initialRawParams.amount),
+  //   unit: getSafeString(initialRawParams.unit),
+  //   nutriment_basis: getSafeString(initialRawParams.nutriment_basis),
+  //   nutriments: JSON.parse(getSafeString(initialRawParams.nutriments)),
+  // };
 
-  const [item] = useState<Food>({ ...initialParams });
+  // const [item] = useState<Food>({ ...initialParams });
 
-  console.log('coso', { item, initialParams });
+  // console.log('coso', { item, initialParams });
 
   return (
     <View style={{ flex: 1 }}>
--- components/common/index.ts
+++ components/common/index.ts
@@ -1,7 +1,5 @@
 export * from './Button';
 export * from './Container';
 export * from './TextInput';
-export * from './HeightInput';
-export * from './WeightInput';
 export * from './RadioOption';
 export * from './ProgressBar';
