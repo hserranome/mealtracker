d6caf9240f37d9fa865396803c7d5a564776f668
Hendy Serrano <hendy@hserrano.me>
2024-09-22 14:16:26 +0200

    chore: rm units and set placeholder submit

--- components/onboarding/OnboardingDataProvider/OnboardingDataProvider.tsx
+++ components/onboarding/OnboardingDataProvider/OnboardingDataProvider.tsx
@@ -1,19 +1,24 @@
-import { createContext, FC, PropsWithChildren, useReducer, useState, useCallback } from 'react';
+import {
+  createContext,
+  FC,
+  PropsWithChildren,
+  useReducer,
+  useState,
+  useCallback,
+  useContext,
+} from 'react';
 
-import { db } from '~/data/database';
-import { NewUser, users } from '~/data/schemas';
+import { ActivityLevel, Goal, Sex } from '~/data/types';
 
 export type OnboardingData = {
-  goal?: NewUser['goal'];
-  activityLevel?: NewUser['activity_level'];
-  sex?: NewUser['sex'];
-  birthYear?: NewUser['birth_year'];
-  heightUnit?: NewUser['height_unit'];
-  weightUnit?: NewUser['weight_unit'];
-  initialHeight?: NewUser['initial_height'];
-  initialWeight?: NewUser['initial_weight'];
-  goalWeight?: NewUser['goal_weight'];
-  weightVarianceRate?: NewUser['weight_variance_rate'];
+  goal?: Goal;
+  activityLevel?: ActivityLevel;
+  sex?: Sex;
+  birthYear?: number;
+  initialHeight?: number;
+  initialWeight?: number;
+  goalWeight?: number;
+  weightVarianceRate?: number;
 };
 
 type OnboardingAction =
@@ -65,24 +70,12 @@ export const OnboardingDataProvider: FC<PropsWithChildren> = ({ children }) => {
   const submit = useCallback(async () => {
     setSubmitting(true);
     try {
-      const newUser: typeof users.$inferInsert = {
-        goal: data.goal,
-        activity_level: data.activityLevel,
-        sex: data.sex,
-        birth_year: data.birthYear,
-        height_unit: data.heightUnit,
-        weight_unit: data.weightUnit,
-        initial_height: data.initialHeight,
-        initial_weight: data.initialWeight,
-        goal_weight: data.goalWeight,
-        weight_variance_rate: data.weightVarianceRate,
-      };
-      return await db.insert(users).values(newUser).returning({ id: users.id });
     } finally {
       setSubmitting(false);
     }
   }, [data]);
 
+  console.log('data', data);
   return (
     <OnboardingDataContext.Provider
       value={{
