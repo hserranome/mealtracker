a675468d01d7beb499ad07aeac471dae2f94ed93
Hendy Serrano <hendy@hserrano.me>
2024-09-26 22:29:55 +0200

    chore: move DB_NAME

--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,3 @@
 export * from './schemas';
 export * from './tinybase';
 export * from './types';
-
-export const DB_NAME = 'local.db';
--- data/tinybase.ts
+++ data/tinybase.ts
@@ -1,8 +1,11 @@
+import { createTools } from 'tinybase/tools/with-schemas';
 import * as TBUIReact from 'tinybase/ui-react/with-schemas';
 import type { WithSchemas } from 'tinybase/ui-react/with-schemas';
 import { createStore } from 'tinybase/with-schemas';
 
 import { tablesSchema, valuesSchema } from './schemas';
+
+export const DB_NAME = 'local.db';
 
 // Cast the whole module to be schema-based
 const TinyBase = TBUIReact as WithSchemas<[typeof tablesSchema, typeof valuesSchema]>;
@@ -10,3 +13,9 @@ export const useTinyBase = () => TinyBase;
 
 // Initialize store instance
 export const tbStore = createStore().setSchema(tablesSchema, valuesSchema);
+
+if (process.env.NODE_ENV === 'development') {
+  const tools = createTools(tbStore);
+  const [dTs, ts, uiReactDTs, uiReactTsx] = tools.getStoreApi(DB_NAME);
+  // TODO: Export to file. We can't do it here.
+}
