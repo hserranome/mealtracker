2ed17b7860fd715dbcb62cc69e03a28e7a9d91db
Hendy Serrano <hendy@hserrano.me>
2024-09-26 22:24:11 +0200

    chore: clean up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'drizzle-kit';
 export default {
-  schema: './data/schemas/index.ts',
-  out: './data/migrations',
   dialect: 'sqlite',
   driver: 'expo',
 } satisfies Config;
