91910620b5260d63bab5b9bfdb5283576d9052e1
Hendy Serrano <hendy@hserrano.me>
2024-10-13 14:19:39 +0200

    chore: cleanup schemas

--- data/schemas/dairies.ts
+++
@@ -1,8 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-export const dairies = sqliteTable('dairies', {
-  id: integer('id').primaryKey({ autoIncrement: true }).unique().notNull(),
-  date: text('date').notNull(),
-});
-export type Dairy = typeof dairies.$inferSelect;
-export type NewDairy = typeof dairies.$inferInsert;
--- data/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './users';
-export * from './dairies';
-export * from './dairies_items';
-export * from './foods';
-export * from './recipes';
-export * from './recipes_items';
-export * from './goals';
