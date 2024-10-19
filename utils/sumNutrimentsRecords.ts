beb3b16774120c72cf5a0438c51b079219c925c1
Hendy Serrano <hendy@hserrano.me>
2024-10-19 19:06:16 +0200

    change: fix non numbers

--- utils/sumNutrimentsRecords.ts
+++ utils/sumNutrimentsRecords.ts
@@ -7,11 +7,20 @@ export const sumNutrimentsRecords = (
 	const result: Nutriments = { ...record1 };
 
 	for (const [key, value] of Object.entries(record2)) {
-		if (key in result) {
-			result[key as keyof Nutriments] += value as number;
-		} else {
-			result[key as keyof Nutriments] = value as number;
+		// Check if the value is a number
+		if (typeof value === "number") {
+			if (key in result) {
+				// Ensure the existing value is also a number before adding
+				if (typeof result[key as keyof Nutriments] === "number") {
+					result[key as keyof Nutriments] += value;
+				} else {
+					result[key as keyof Nutriments] = value;
+				}
+			} else {
+				result[key as keyof Nutriments] = value;
+			}
 		}
+		// If the value is not a number, we skip it
 	}
 
 	return result;
